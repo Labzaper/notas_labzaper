530ef9381d03297e796088f9de35d185a751d4d7
Guillermo Cruces <cruces.perzabal@gmail.com>
2015-08-26 15:04:10 -0500

    commit primero

---
+++ javascript.js
@@ -0,0 +1,239 @@
+Javascript
+
+"Variable naming rules."
+
+	*      A variable may include only the letters a-z, A-Z, 0-9, the $ symbol and the underscore.
+	*      No other characters, such as spaces or punctuation, are allowed in a variable name.
+	*      Are case-sensitive.
+	*      There is no set limit on variable name lengths.
+
+	String concatenation
+	document.write("You have " + messages + "messages.");
+
+	name= "James";
+	name += "Dean";
+
+	Function Scope Defining what is accesible in code and where.
+
+
+"Variable Scope"
+
+	function test()
+	{
+	     a = 123          // global scope
+	     var b = 456     // local scope
+	     if (a == 123) var c = 789     // local scope 
+	}
+
+
+"Scope" 
+	var x = 2000;
+
+	function someFunc() {
+		var y = 12;
+		return y;
+	}
+
+	var z = x + y; // Invalid use of y
+
+	var x = x + someFunc(); // z == 2012
+
+
+"Functions in Functions"
+	function outerFunction(n) {
+		function innerFunction() {
+			return n * n;
+		}
+		return innerFunction();
+	}
+
+	var x = outerFunction(4); // x == 16
+
+	// innerFunction cannot be called directly
+
+
+"Immediate Functions"
+	(function() {...} ());
+	or
+	(function() {...} )();
+
+
+"Module Pattern"
+	var mod = (function() {
+		var m = 2000, c = 0, d = 10, y = 2;
+		return {
+			getHiddenYear : function() {
+				return m + c + d + y;
+			}
+		}
+	}());
+	var x = mod.getHiddenYear(); // x == 2012
+
+
+"With"
+
+	string = "The quick brown fox jumps over the lazy dog";
+
+	with (string) 
+	{ 
+	     document.write("The string is " + length + " characters<br>");      
+	     document.writeln("In uppercase it's: " + toUpperCase()) 
+	}
+
+
+"Functions - The arguments array" 
+
+
+	* Pass more than n items to the function:
+
+	displayItems("oso", "perro", "gato");
+
+	function displayItems() 
+	{ 
+	     for (j=0; j < displayItems.arguments.length; ++j) 
+	     { 
+	          document.write(displayItems.arguments[j] + "<br>") 
+	     } 
+	}
+
+
+	* Cleaning up a full name:
+
+	function fixNames("tHE", "dAllAs", "coWBoys");
+
+	function fixNames()
+	{
+	     var s = ""; 
+	     for (j=0; j < fixNames.arguments.length; ++j) 
+	          s += fixNames.arguments[j].charAt(0).toUpperCase() + fixNames.arguments[j].substr(1).toLowerCase() + " "; 
+	          return s.substr(0, s.length - 1) 
+	}
+
+
+"Arrays"
+
+	toys = ['bat', 'ball', 'puzzle']; 
+	toys = Array('bat', 'ball', 'puzzle');
+
+	arrayName = new Array();
+
+	arrayName = [];
+
+	arrayName.push("Element 1")
+	arrayName.push("Element 2")
+
+	arrayName[0] = "Element 1"
+	arrayName[1] = "Element 2"
+
+	// Creating, building and printing an array
+	numbers = []
+
+	numbers.push("One")
+	numbers.push("Two")
+	numbers.push("Three")
+
+	for(j = 0; j < numbers.length; ++j) { document.write("Element " + j + " = " + numbers[j] + "<br>")}
+
+
+"Associative Arrays"
+
+	// Creating
+	balls = {"golf":"Golf balls, 6", "tennis":"Tennis balls, 3", "soccer":"Soccer ball, 1", "ping":"Ping Pong balls, 1 doz"}
+
+	// Displaying
+	for(ball in balls){document.write(ball + " = " + balls[ball] + "<br>")}
+
+
+"Array Methods"
+
+	* concat
+		fruit = ['banana', 'apple', 'grapes']
+		veg = ['onion', 'tomato', 'avocado']
+
+		document.write(fruit.concat(veg));
+		list = fruit.concat(veg);
+
+	* join
+		pets = ['cat', 'dog', 'rabbit', 'hamster']
+
+		document.write(pets.join() + "<br>"); 
+		document.write(pets.join(' ') + '<br>');
+		document.write(pets.join(':') + "<br>");
+
+	* pop and push
+		sports = ['football', 'hockey', 'basketball']
+		sports.push('baseball')
+		sports.pop()   // Removes the last element
+
+	* reverse()
+
+	* sort()    // Alphabetical sort
+		console.log(fruit.sort());
+
+	* sort.reverse()
+
+	* sort numeric values
+		numbers.sort( function(a,b){return a - b} );
+		numbers.sort( function(a,b){return b - a} );      // Descending
+
+	* slice
+		fruit = fruit.slice(0,2);     // ["apple", "banana"]
+
+	* splice
+       fruit = fruit.splice(1, 2, "melon","grape");
+       console.log(fruit);
+
+	* map
+		fruit = fruit.map(function (i) { return i.toUpperCase() });     // convert all letters in upercase
+
+	* filter
+		fruit = fruit.filter(function (i) {return i[0] === "a"; });     // apple
+
+	* every
+		console.log(fruit.every( function (i) { return i[0] === "a"; }));     // returns False. It checks that Every element begins with a "a" letter.
+		console.log(fruit.every( function (i) { return i.length > 0; }));     // returns True. 
+
+	* some
+        console.log(fruit.every( function (i) { return i[0] === "a"; }));     // returns True. 
+
+	* forEach
+		fruit.forEach(function (i) {
+		     ...
+		}
+
+
+"Methods"
+     
+	var ops = {
+	     add : function addNumbers(n1, n2) {
+	          return n1 + n2;
+	     }
+	};
+
+	var x = ops.add(23,12);     // x = 35
+
+"Objects"
+	Instantiate an object
+
+		* "Object Initializer syntax"
+			var dog = {
+				breed: 	"German Shepherd"
+				bark: 	function() { console.log("woof"); }
+			};
+	                    
+
+		* "JSON object notation"
+			var dog = {};
+			dog.breed = "German Shepherd";
+			dog.bark = function() { console.log("woof"); };
+
+
+
+
+
+
+
+
+
+
+	
---
+++ php_mysql.php
@@ -0,0 +1,226 @@
+CSS
+
+Selectors
+
+	* Type selectors
+	* .class selectors
+	* #id selectors
+	* [Attribute] selector
+
+		*      [required] {color:red;}
+		*      [data-author=psmith] {color:blue;]
+
+	* :pseudo-class and :pseudo-element selectors
+	 
+		*      p::first-letter {color:red;}
+		*      p::first-line {color:red;}
+		*      p:hover {color:red;}
+
+	* the universal selector
+	* selector chains
+
+
+Combinators
+
+	*      descendant combinator
+	*      child > combinator
+	*      general ~ sibling
+	*      adjacent + sibling
+
+Opacity
+p { opacity: 0.5; }
+
+box-sizing: (content-box | border-box); 
+
+.centered
+{
+  display: table-cell;
+  min-height: 200px;
+  min-width: 200px;
+  text-align: center;
+  vertical-align: middle; 
+}
+
+
+/********-----------------------------------------------------------------------------------------------------------------********\
+
+PHP
+
+Static variables are accessible only within the function that declared them but retain their value over multiple calls.
+
+Passing by Reference 
+     Prefacing a variable with the & symbol tells the parser to pass a reference to the variable's value, not the value itself.
+
+$this is used to access the current object's properties.
+
+Static Methods. It is called on a class, not on an object. It has no access to any object properties.
+A property declared static cannot be directly accessed within an instance of a class, but a static method can.
+
+:: Scope resolution operator.
+
+Accessing outside of the class:
+     MyClass::$myProperty;
+
+Inside os the class:
+     self::$myProperty;
+
+Subclass constructor. When extend a class and declare its own constructor, PHP will not automatically call the constructor method of the parent class.
+     class Tiger extends wildCat
+     {
+          function __construct()
+          {
+               parent::__construct();  // Call parent constructor first. 
+          }
+     }
+
+Final Methods. To prevent a subclass from overriding a superclass method, you can use the final keyword. 
+Final Class. This is a class you can´t heritage from.
+
+Extract function. Turn the key/values pairs from an array into PHP variables.
+Compact function. Create an array from variables.
+
+Cookies
+     Setting a cookie syntax:
+      setcookie(name, value, expire, path, domain, secure, httponly);
+
+     Example:
+      setcookie('username', 'Labzaper', time() + 60 * 60 * 24 * 7, '/');
+
+     Access:
+      if (isset($_COOKIE['username']) $username = $_COOKIE['username']);
+
+     Destroy:
+      setcookie('username', 'Labzaper', time() - 2592000, '/');
+      You must set a date in the past. The 2592000 time value is equivalent to one month.
+
+Tips:
+* It is considering good practice to place functions and class definitions toward the end of a file.
+* VirtualHost
+* Vagrant
+* puphpet
+
+/********-----------------------------------------------------------------------------------------------------------------********\
+
+MySQL
+
+  * mysql -u root  
+  * mysql -u guillermo.cruces -p;
+  * mysql -uhomestead -psecret
+
+  * mysql -h nombre_servidor -u nombre_usuario -p 
+
+Si deseamos conectarnos a la base de datos en local y con nombre de usuario root tendríamos que escribir:
+
+  * mysql -h localhost -u root -p 
+
+  * show databases;
+  * use databaseName;
+  * create database [database];
+  * show tables;
+  * describe [table];   -- Show table structure
+
+SQL commands and keywords are case-insensitive.
+
+Add user
+GRANT PRIVILEGES ON database.object TO 'username'@'hostname' IDENTIFY BY 'password';
+
+     i.e. GRANT ALL ON publications.* TO 'guillermo.cruces'@'localhost' IDENTIFIED BY 'cooper'
+
+Add a key column
+ALTER TABLE classics ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT KEY; 
+
+Delete a column
+ALTER TABLE classics DROP id;
+
+Rename a table
+ALTER TABLE classics RENAME newName;
+
+Rename a column
+ALTER TABLE classics CHANGE type category VARCHAR(16);
+     
+     The CHANGE keyword requires the datatype to be specified.
+
+Drop a table
+DROP TABLE classics;
+
+Changing the datatype of a column
+ALTER TABLE classics MODIFY year SMALLINT;
+
+Adding Index to a column
+ALTER TABLE classics ADD INDEX(author(20));
+     The (20) is limiting the index to only the first 20 characters.
+
+Adding a FULLTEXT index
+ALTER TABLE classics ADD FULLTEXT(author, title);
+
+Adding a Primary Key
+ALTER TABLE classics ADD isbn CHAR(13) PRIMARY KEY;
+
+CREATE TABLE classics (
+     author VARCHAR(128),
+     title VARCHAR(128),
+     category VARCHAR(16), 
+     year SMALLINT) ENGINE MyISAM;
+
+Insert rows
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9780598113101','Mark Twain', 'Tom Sawyer', 'Fiction','1876');
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9780598113102','Jane Austen', 'Pride and Prejudice', 'Fiction','1811');
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9670598111245','Charles Darwin', 'The origin of species', 'Non-Fiction','1856');
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9734598113102','William Shakespeare', 'Romeo and Juliet', 'Play','1594');
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9629698111245','Charles Darwin', 'The origin of species 2', 'Non-Fiction','1880');
+INSERT INTO classics (isbn, author, title, category, year) VALUES ('9851698111245','Charles Dickens', 'The old curiosity shop', 'Fiction','1870');
+
+Select
+SELECT COUNT(*) FROM classics;
+SELECT DISTINCT author FROM classics;
+SELECT DISTINCT author FROM classics WHERE author LIKE 'Charles%';
+
+MATCH AGAINST. Is used to make natural-languages searches in columns that have been given a FULLTEXT index. Case insensitive.
+     SELECT author, title, year FROM classics WHERE MATCH (author, title) AGAINST ('Tom Sawyer');
+
+SELECT author, title FROM classics WHERE MATCH (author, title) AGAINST ('+Charles -Species' IN BOOLEAN MODE);
+
+SELECT author, title FROM classics WHERE MATCH (author, title) AGAINST (' "origin of" ' IN BOOLEAN MODE);
+     Double quotation marks override stopwords.
+
+UPDATE classics SET category='Classic fiction' WHERE category = 'Fiction';
+
+Delete. Remove a row from a table.
+
+SELECT author, title, year FROM classics ORDER BY author ASC, year DESC;
+
+SELECT category, COUNT(author) FROM classics GROUP BY category;
+
+JOIN
+
+SELECT name, author, title from customers, classics
+WHERE customers.isbn = classics.isbn;
+
+INSERT INTO customers (name, isbn) VALUES
+     ('Joe Bloggs', '9780598113101'),
+     ('Mary Smith', '9780598113102'),
+     ('Jack Wilson', '9629698111245');
+
+JOIN...ON
+SELECT name, author, title FROM customers
+JOIN classics ON customers.isbn = classics.isbn;
+
+AS
+     SELECT name, author, title 
+     FROM customers AS cust, classics AS class
+     WHERE cust.isbn = class.isbn;
+
+EXPLAIN Analyze and optimize querys.
+     EXPLAIN SELECT * FROM accounts;
+
+Tips: 
+
+  * The recommended style is to use uppercase for SQL commands and keywords.
+  * Use lowercase for tables.
+  * Always initialize variables by security purposes. Also is a good programming practice because you can comment each initialization to show what each variable is for. 
+
+
+CREATE DATABASE demo;
+USE demo;
+GRANT ALL ON demo.* TO guillermo@localhost IDENTIFIED BY 'laravelmx';
+FLUSH PRIVILEGES;
